857b4b5028c0bf9a8f21e395d612fd93b1520775
Coya-yan <2803152091@qq.com>
2021-01-18 16:12:38 +0800

    Create your first learning project

---
+++ rust-world/target/debug/deps/rust_world.d
@@ -0,0 +1,5 @@
+/Users/collin/Documents/Development/Rust/rust-world/target/debug/deps/rust_world: src/main.rs
+
+/Users/collin/Documents/Development/Rust/rust-world/target/debug/deps/rust_world.d: src/main.rs
+
+src/main.rs:
---
+++ rust-world/target/debug/rust-world.d
@@ -0,0 +1,1 @@
+/Users/collin/Documents/Development/Rust/rust-world/target/debug/rust-world: /Users/collin/Documents/Development/Rust/rust-world/src/main.rs
